7b82226d4ab2eb7de729461e428bf22f6418f02d
Rotunda6209 <105428998+Rotunda6209@users.noreply.github.com>
2022-05-27 18:31:10 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,12 @@
 #setup ubuntu after fresh install
 #information taken from here https://www.addictivetips.com/ubuntu-linux-tips/make-ubuntu-post-installation-script/
 
-
+#good practice
 sudo apt update
 sudo apt upgrade
 
+#install flatpak as per flatpak's intstructions
 sudo apt install flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# 
